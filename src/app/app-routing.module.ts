f23a1bae491ec9217a15032aeda25849474294a8
th257 <vignesharun619@gmail.com>
2020-12-08 16:21:10 +0530

    sucessfullt commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,42 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { MarketingComponent } from './marketing/marketing.component'
+import { PlanningComponent } from './planning/planning.component';
+import { ProductionComponent } from './production/production.component';
+import { PurchaseComponent } from './purchase/purchase.component';
+import { StockComponent } from './stock/stock.component';
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'/home',
+    pathMatch:'full'
+  },
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'marketing',
+    component:MarketingComponent
+  },
+  {
+    path:'planning',
+    component:PlanningComponent
+  },
+  {
+    path:'production',
+    component:ProductionComponent
+  },
+  {
+    path:'purchase',
+    component:PurchaseComponent
+  },
+  {
+    path:'stock',
+    component:StockComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
